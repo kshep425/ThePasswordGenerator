1be6e4263b125cda0ecc0215c06cac4477fc0f59
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-10-30 20:49:16 -0400

    Added alert if copy text is selected but there wasn't a generated password.

--- assets/js/copy_text.js
+++ assets/js/copy_text.js
@@ -4,14 +4,16 @@
     function myFunction() {
         // get text from generated-password
         var copy_text = document.getElementById("generated-password");
-        console.log(copy_text)
+        console.log(copy_text);
         var the_password_to_copy = copy_text.innerText
-        console.log(the_password_to_copy)
-        newClip = the_password_to_copy
-        console.log("newClip: " + newClip)
-        navigator.clipboard.writeText(newClip).then(function() {
+        console.log(the_password_to_copy);
+        if (the_password_to_copy === ""){
+            alert("Please click 'Generate Password' button first.")
+            return
+        }
+        navigator.clipboard.writeText(the_password_to_copy).then(function() {
           /* clipboard successfully set */
-          console.log("copied to clipboard: " + newClip)
+          console.log("copied to clipboard: " + the_password_to_copy)
         }, function() {
           /* clipboard write failed */
           console.log("as usual it didn't work...please report the error to kshep425@yahoo.com")
